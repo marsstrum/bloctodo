5cb287d1d6c111691ada714594f5756b54341a14
David French <marsstrum@gmail.com>
2014-10-18 00:26:58 +0000

    error clean ups

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Bloctodo::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { host: 'marsstrum-bloctodo.herokuapp.com'}
 end
